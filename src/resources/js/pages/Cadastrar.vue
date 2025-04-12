120ced99a401b8474970ff31e9dcb90a988d046b
HeroDestiny <paulo_phdestiny@hotmial.com>
2025-04-12 11:26:56 -0300

    feat: adicionado novos campos ao cadastro

--- src/resources/js/pages/Cadastrar.vue
+++ src/resources/js/pages/Cadastrar.vue
@@ -1,6 +1,5 @@
 <template>
-  <AppLayout :breadcrumbs="breadcrumbs">
-
+  <AppLayout>
     <Head title="Cadastrar" />
 
     <Card class="w-full p-6">
@@ -8,30 +7,60 @@
         <CardTitle class="text-2xl">Criar Conta</CardTitle>
         <CardDescription>Preencha seus dados abaixo para se registrar.</CardDescription>
       </CardHeader>
+
       <CardContent>
         <form @submit.prevent="onSubmit" class="space-y-4">
-          <div>
-            <Label for="name">Nome</Label>
-            <Input id="name" v-model="form.name" placeholder="Digite seu nome" required />
+
+          <div class="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <div>
+              <Label for="name">Nome</Label>
+              <Input id="name" v-model="form.name" placeholder="Digite seu nome"  />
+            </div>
+
+            <div>
+              <Label for="surname">Sobrenome</Label>
+              <Input id="surname" v-model="form.surname" placeholder="Digite seu sobrenome"  />
+            </div>
+
+            <div>
+              <Label for="birthdate">Data de Nascimento</Label>
+              <Input id="birthdate" v-model="form.birthdate" type="date"  />
+            </div>
+
+            <div>
+              <Label for="cpf">CPF</Label>
+              <Input id="cpf" v-model="form.cpf" placeholder="Digite seu CPF"  />
+            </div>
+
+            <div>
+              <Label for="role">Cargo</Label>
+              <Input id="role" v-model="form.role" placeholder="Digite seu cargo"  />
+            </div>
+
+            <div>
+              <Label for="education">Escolaridade</Label>
+              <Input id="education" v-model="form.education" placeholder="Digite sua escolaridade"  />
+            </div>
+
+            <div class="md:col-span-2">
+              <Label for="motherName">Nome da Mãe</Label>
+              <Input id="motherName" v-model="form.motherName" placeholder="Digite o nome da sua mãe"  />
+            </div>
+
+            <div class="md:col-span-2">
+              <Label for="email">Email</Label>
+              <Input id="email" v-model="form.email" type="email" placeholder="Digite seu email"  />
+            </div>
+
           </div>
 
-          <div>
-            <Label for="email">Email</Label>
-            <Input id="email" v-model="form.email" type="email" placeholder="Digite seu email" required />
-          </div>
-
-          <div>
-            <Label for="password">Senha</Label>
-            <Input id="password" v-model="form.password" type="password" placeholder="Digite sua senha" required />
-          </div>
-
-          <Button type="submit" class="w-full">
+          <Button type="submit" class="w-full mt-6">
             Registrar
           </Button>
+
         </form>
       </CardContent>
     </Card>
-
   </AppLayout>
 </template>
 
@@ -42,38 +71,48 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 import { Button } from '@/components/ui/button'
 import AppLayout from '@/layouts/AppLayout.vue'
-import { type BreadcrumbItem } from '@/types'
 import { Head } from '@inertiajs/vue3'
-
-
-const breadcrumbs: BreadcrumbItem[] = [
-  {
-    title: 'Dashboard',
-    href: '/dashboard',
-  },
-];
+import { toast } from 'vue-sonner'
 
 interface RegisterForm {
   name: string
+  surname: string
+  birthdate: string
+  cpf: string
+  role: string
+  education: string
+  motherName: string
   email: string
-  password: string
 }
 
 const form = ref<RegisterForm>({
   name: '',
+  surname: '',
+  birthdate: '',
+  cpf: '',
+  role: '',
+  education: '',
+  motherName: '',
   email: '',
-  password: '',
 })
 
 const onSubmit = () => {
   console.log('Dados do formulário:', form.value)
 
-  alert("sucesso")
+  toast('Cadastro realizado com sucesso!', {
+    description: `Bem-vindo(a), ${form.value.name}!`
+  })
 
+  // Limpar formulário
   form.value = {
     name: '',
+    surname: '',
+    birthdate: '',
+    cpf: '',
+    role: '',
+    education: '',
+    motherName: '',
     email: '',
-    password: '',
   }
 }
 </script>
