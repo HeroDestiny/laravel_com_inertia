a79dd2d6d023f71c717f439889ed40782c9e84fe
Paulo Henrique Soares Lima e Silva <59665236+HeroDestiny@users.noreply.github.com>
2025-07-22 17:17:19 +0000

    chore: correção na PacienteFactory

--- src/database/factories/PacienteFactory.php
+++ src/database/factories/PacienteFactory.php
@@ -13,30 +13,30 @@ use Override;
  */
 class PacienteFactory extends Factory
 {
-  /**
-   * The name of the factory's corresponding model.
-   *
-   * @var class-string<\Illuminate\Database\Eloquent\Model>
-   */
-  protected $model = Paciente::class;
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var class-string<\Illuminate\Database\Eloquent\Model>
+     */
+    protected $model = Paciente::class;
 
-  /**
-   * Define the model's default state.
-   *
-   * @return array<string, mixed>
-   */
-  #[Override]
-  public function definition(): array
-  {
-    return [
-      'name' => $this->faker->firstName,
-      'surname' => $this->faker->lastName,
-      'birthdate' => $this->faker->date('Y-m-d', '2000-01-01'),
-      'cpf' => $this->faker->unique()->numerify('###########'), // 11 digits
-      'role' => $this->faker->optional()->jobTitle,
-      'education' => $this->faker->optional()->randomElement(['Fundamental', 'Médio', 'Superior', 'Pós-graduação']),
-      'mother_name' => $this->faker->name('female'),
-      'email' => $this->faker->unique()->safeEmail,
-    ];
-  }
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    #[Override]
+    public function definition(): array
+    {
+        return [
+            'name' => $this->faker->firstName,
+            'surname' => $this->faker->lastName,
+            'birthdate' => $this->faker->date('Y-m-d', '2000-01-01'),
+            'cpf' => $this->faker->unique()->numerify('###########'), // 11 digits
+            'role' => $this->faker->optional()->jobTitle,
+            'education' => $this->faker->optional()->randomElement(['Fundamental', 'Médio', 'Superior', 'Pós-graduação']),
+            'mother_name' => $this->faker->name('female'),
+            'email' => $this->faker->unique()->safeEmail,
+        ];
+    }
 }
